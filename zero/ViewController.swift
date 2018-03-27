ce38d3d379efc45eca4b2da9115d7f5260bfbde6
jin xie <jin.xie@alliants.com>
2018-03-27 15:50:59 +0100

    Initial Commit

---
+++ zero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zero
+//
+//  Created by jinfeng xie on 27/03/2018.
+//  Copyright © 2018 Alliants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

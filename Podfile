008a61bcb5e877bf4c7ccc100aefcb38fe7de612
jin xie <jin.xie@alliants.com>
2018-03-28 22:26:25 +0100

    SearchBar updates tableView with RxSwift

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, "11.2"
+workspace "zero.xcworkspace"
+use_frameworks!
+
+target "zero" do
+  pod "RxSwift", "~> 4.0"
+  pod "RxCocoa", "~> 4.0"
+  pod 'RxDataSources', '~> 3.0'
+
+  project "zero"
+end
--- zero/ViewController.swift
+++ zero/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+struct TableViewSection {
+    var header: String
+    var items: [Item]
+}
+
+extension TableViewSection : AnimatableSectionModelType {
+    typealias Item = String
+    
+    var identity: String {
+        return header
+    }
+    
+    init(original: TableViewSection, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let allCities = ["Shenzhen", "Guangzhou"]
+    private let disposeBag = DisposeBag()
+    private var searchedCities = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let dataSource = RxTableViewSectionedReloadDataSource<TableViewSection>(
+            configureCell: { ds, tv, ip, item in
+                let cell = tv.dequeueReusableCell(withIdentifier: "cityPrototypeCell") ?? UITableViewCell(style: .default, reuseIdentifier: "cityPrototypeCell")
+                cell.textLabel?.text = item
+                
+                return cell
+            },
+            titleForHeaderInSection: { ds, index in
+                return "Cities"
+            }
+        )
+        
+        let searchedCities = searchBar.rx.text.orEmpty.asDriver(onErrorJustReturn: "")
+            .flatMapLatest { [unowned self] query in
+                return Observable.just(self.allCities.filter { $0.hasPrefix(query) }).asDriver(onErrorJustReturn: [""])
+            }
+        
+        searchedCities
+            .map { [TableViewSection(header: "searchedResult", items: $0)] }
+            .drive(tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
